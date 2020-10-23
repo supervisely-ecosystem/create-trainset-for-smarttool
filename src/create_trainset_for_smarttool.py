7e9c15120bf1db444cc74a9ba493b0c89eac0fe1
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-10-23 11:46:52 +0300

    gui - split info in progress

--- src/create_trainset_for_smarttool.py
+++ src/create_trainset_for_smarttool.py
@@ -11,6 +11,8 @@ TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 
+project = None
+total_images_count = None
 
 @my_app.callback("apply_to_random_image")
 @sly.timeit
@@ -22,21 +24,52 @@ def do(api: sly.Api, task_id, context, state, app_logger):
     print("123")
     pass
 
+@my_app.callback("count_train_val_split")
+def do(api: sly.Api, task_id, context, state, app_logger):
+    train_percent = state["trainPercent"]
+    if total_images_count is None:
+        raise ValueError("Total images count is not defined")
+        return
+    train_images_count = min(1, total_images_count * train_percent / 100)
+    val_images_count = total_images_count - train_images_count
+    fields = [
+        {"field": "data.trainImagesCount", "payload": train_images_count},
+        {"field": "data.valImagesCount", "payload": val_images_count},
+    ]
+    api.task.set_fields(task_id, fields)
+
 def main():
     api = sly.Api.from_env()
+    global project, total_images_count
+
     project = api.project.get_info_by_id(PROJECT_ID)
+    train_percent = 80
+    total_images_count = api.project.get_images_count(project.id)
+    train_images_count = min(1, total_images_count * train_percent / 100)
+    val_images_count = total_images_count - train_images_count
+    split_table = [
+        {"name": "total", "count": total_images_count},
+        {"name": "train", "count": train_images_count},
+        {"name": "val", "count": val_images_count}
+    ]
 
     data = {
         "projectId": project.id,
         "projectName": project.name,
         "projectPreviewUrl": api.image.preview_url(project.reference_image_url, 100, 100),
         "progress": 0,
-        "started": False
+        "started": False,
+        "totalImagesCount": total_images_count,
+        "trainImagesCount": train_images_count,
+        "valImagesCount": val_images_count,
+        "splitTable": split_table
     }
 
     state = {
+        "trainPercent": train_percent,
         "filterPercent": 5,
-        "paddingRange": [5, 10]
+        "paddingRange": [5, 10],
+        "minPointsCount": 0
     }
 
     initial_events = [
